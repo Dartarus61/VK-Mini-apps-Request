cb8d028af59e9fe1b299efa6ecdf2c6583d3b6db
Dartar <07kirgame@gmail.com>
2023-03-30 19:54:59 +0300

    fix: refactor

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from 'src/models/user.model';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   providers: [UserService],
   exports: [UserService],
-  imports: [SequelizeModule.forFeature([User])]
+  imports: [SequelizeModule.forFeature([User])],
+  controllers: [UserController]
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,37 +4,44 @@ import { User } from 'src/models/user.model';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
 
-    async createUser(userId: number) {
-        const user = await this.userRepository.findOne({where: {
-            userId
-        }})
+  async createUser(userId: number) {
+    const user = await this.userRepository.findOne({
+      where: {
+        userId,
+      },
+    });
 
-        if (user) {
-            throw new HttpException('User is exist', HttpStatus.BAD_REQUEST)
-        }
-
-        return this.userRepository.create({userId})
+    if (user) {
+      throw new HttpException('User is exist', HttpStatus.BAD_REQUEST);
     }
 
-    async getUserById(id: number) {
-        const user = await this.userRepository.findByPk(id, {include: {all: true}})
+    return this.userRepository.create({ userId });
+  }
 
-        if (!user) {
-            throw new HttpException('User not found', HttpStatus.BAD_REQUEST)
-        }
+  async getUserById(id: number) {
+    const user = await this.userRepository.findByPk(id, {
+      include: { all: true },
+    });
 
-        return user
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
     }
 
-    async getUserByVkUserId(userId: number) {
-        const user = await this.userRepository.findOne({where: {userId}, include: {all: true}})
+    return user;
+  }
 
-        if (!user) {
-            throw new HttpException('User not found', HttpStatus.BAD_REQUEST)
-        }
+  async getUserByVkUserId(userId: number) {
+    const user = await this.userRepository.findOne({
+      where: { userId },
+      include: { all: true },
+    });
 
-        return user
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
     }
+
+    return user;
+  }
 }
