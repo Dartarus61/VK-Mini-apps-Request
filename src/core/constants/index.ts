3c759e57b55a132ad3adb51175633b84d2ef2147
Dartar <07kirgame@gmail.com>
2023-04-13 01:38:26 +0300

    23

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -24,12 +24,14 @@ export const CLAIM_TEXT = (name: string, id: number, uri: string) =>
 export const KEYBOARD_FOR_CLAIM = {
   inline: true,
   buttons: [
-    {
-      action: {
-        type: 'callback',
-        label: 'Заблокировать',
-        payload: '{uri:333}',
+    [
+      {
+        action: {
+          type: 'callback',
+          label: 'Заблокировать',
+          payload: '{uri:333}',
+        },
       },
-    },
+    ],
   ],
 };
