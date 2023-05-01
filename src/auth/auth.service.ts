ddcda7275a635beb06c759ebe3d40b2a76e74ea0
Dartar <07kirgame@gmail.com>
2023-05-01 12:56:17 +0300

    fix: merge confilcts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
 
   async signUpIn(token: string) {
     console.log(token);
-    
+
     const verifyUser = this.verifyLaunchParams(token, PRIVATE_KEY);
 
     if (!verifyUser) {
@@ -38,49 +38,18 @@ export class AuthService {
       );
     }
 
-    const userId = this.getUserIdFromURI(dto.uri);
-
-    if (userId !== dto.userId) {
-      throw new HttpException(
-        'User ID is not an equal',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
-    let user = await this.userService.getUserByVkUserId(this.getUserIdFromURI(token));
+    let user = await this.userService.getUserByVkUserId(
+      this.getUserIdFromURI(token),
+    );
 
     if (!user) {
       user = await this.userService.createUser(this.getUserIdFromURI(token));
     }
 
-
     return 'successul';
-  }
- //TODO поменять payload токена, добавить проверку ид из строки и из userId в других методах
-
-
-  verifyUserId(token: string) {
-    return this.verifyLaunchParams(token, PRIVATE_KEY);
-  }
-
-  getUserIdFromURI(token: string) {
-    let tokenUserId = token.match(new RegExp('vk_user_id=\\d*', 'gm'));
-
-    if (tokenUserId == null) {
-      throw new HttpException(
-        'URI is invalid while get user from uri',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
-    const userId = tokenUserId[0].split('=')[1];
-    console.log(+userId);
-
-    return +userId;
   }
 
   async getUserData(token: string) {
-
     const verifyUser = this.verifyLaunchParams(token, PRIVATE_KEY);
 
     if (!verifyUser) {
@@ -93,7 +62,6 @@ export class AuthService {
     const user = await this.userService.getUserByVkUserId(
       this.getUserIdFromURI(token),
     );
-
 
     return user;
   }
@@ -122,19 +90,19 @@ export class AuthService {
   }
 
   verifyUserId(token: string) {
-    return this.verifyLaunchParams(token, PRIVATE_KEY)
+    return this.verifyLaunchParams(token, PRIVATE_KEY);
   }
 
   getUserIdFromURI(token: string) {
-    let tokenUserId = token.match('/vk_user_id=\d{4,}/gm')
+    let tokenUserId = token.match('/vk_user_id=d{4,}/gm');
 
-    if (tokenUserId == null){
-      throw new HttpException('URI is invalid', HttpStatus.BAD_REQUEST)
+    if (tokenUserId == null) {
+      throw new HttpException('URI is invalid', HttpStatus.BAD_REQUEST);
     }
 
-    const userId = tokenUserId[0].split('=')[1]
+    const userId = tokenUserId[0].split('=')[1];
 
-    return +userId
+    return +userId;
   }
 
   private verifyLaunchParams(
